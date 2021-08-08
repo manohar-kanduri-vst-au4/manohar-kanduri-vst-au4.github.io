2171a6365516e2610feda68dd995f06e1ebd4642
MANOHAR KANDURI <manoharkanduri@gmail.com>
2021-08-08 13:50:03 +0530

    Update Footer.js
    copyright removed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,8 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
+//   let date = new Date();
+//   let year = date.getFullYear();
   return (
     <Container fluid className="footer">
       <Row>
@@ -16,7 +16,7 @@ function Footer() {
           <h3>Designed and Developed by Kanduri Manohar</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>copyright © {year} </h3>
+//           <h3>copyright © {year} </h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
